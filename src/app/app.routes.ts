1d48c5a2489bc1cd688d452db73934a2b6c03472
mifag <mifag92@rambler.ru>
2025-05-03 15:38:52 +0300

    Add Gallery and others.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { GalleryComponent } from './gallery/gallery.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 import { EventsComponent } from './events/events.component';
 import { OilComponent } from './gallery/oil/oil.component';
 import { PastelComponent } from './gallery/pastel/pastel.component';
@@ -12,6 +13,7 @@ export const routes: Routes = [
     { path: 'contacts', component: ContactsComponent },
     { path: 'about', component: AboutComponent },
     { path: 'events', component: EventsComponent },
+    { path: 'home', component: HomeComponent },
     { path: 'gallery/oil', component: OilComponent },
     { path: 'gallery/pastel', component: PastelComponent },
     { path: 'gallery/sketches', component: SketchesComponent },
